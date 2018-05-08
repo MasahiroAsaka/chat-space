5c0e5b33fff91085e5b6090783cdec9de233886e
MasahiroAsaka <pedestrem.paradiso@gmail.com>
2018-05-08 18:27:28 +0900

    Add Send Message function
    メッセージ送信機能の実装

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -1,6 +1,28 @@
 class MessageController < ApplicationController
 
-  def index
-
+  def new
+    @message = Message.new(message_params)
   end
+
+  def create
+    if @message = Message.save(message_params)
+      redirect_to root_path, notice: "メッセージを投稿しました。"
+    else
+      render :new
+    end
+  end
+
+  def show
+    @message = Message.find(set_message)
+  end
+
+ private
+  def message_params
+    params.require(:message).permit(:text, {:user_ids => []})
+  end
+
+  def set_message
+    @message = Message.find(params[:id])
+  end
+
 end
