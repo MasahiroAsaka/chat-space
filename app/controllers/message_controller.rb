62e05366174ee621b074a24254e04843e2bbdec0
MasahiroAsaka <pedestrem.paradiso@gmail.com>
2018-05-09 12:28:22 +0900

    Add Send Message function
    メッセージ送信機能の実装

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -1,14 +1,19 @@
 class MessageController < ApplicationController
+  before_action :set_group
 
-  def new
-    @message = Message.new(message_params)
+  def index
+    @message = Message.new
+    @messages = @group.messages.includes(:user)
   end
 
   def create
-    if @message = Message.save(message_params)
-      redirect_to root_path, notice: "メッセージを投稿しました。"
+    @message = @group.messages.new(message_params)
+    if @message.save
+      redirect_to group_messages_path(@group), notice: "メッセージが送信されました。"
     else
-      render :new
+      @messages = @group.messages.includes(:user)
+      flash.now[:alert] = "メッセージを入力してください。"
+      render :index
     end
   end
 
@@ -18,11 +23,11 @@ class MessageController < ApplicationController
 
  private
   def message_params
-    params.require(:message).permit(:text, {:user_ids => []})
+    params.require(:message).permit(:text, :image).merge(user_id: current_user.id)
   end
 
-  def set_message
-    @message = Message.find(params[:id])
+  def set_group
+    @group = Group.find(params[:group_id])
   end
 
 end
