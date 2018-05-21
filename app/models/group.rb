fcd8b5d498cf4e799f43965dc11e99c91a604ab1
MasahiroAsaka <pedestrem.paradiso@gmail.com>
2018-05-21 15:22:35 +0900

    #5 Desynchronized Message function
    group.rb 画像とコメントが両方投稿された場合の条件を分け

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,8 +5,13 @@ class Group < ApplicationRecord
   validates :name, presence: true
 
   def show_last_message
-    if (last_message = messages.last).present?
-      last_message.text? ? last_message.text : '画像が投稿されています'
+    last_message = messages.last
+    if last_message.present?
+      if last_message.image.present? && last_message.text.present?
+          last_message.text + ' 画像も投稿されています。'
+      else last_message.present?
+        last_message.text? ? last_message.text : '画像が投稿されています.'
+      end
     else
       'まだメッセージはありません。'
     end
