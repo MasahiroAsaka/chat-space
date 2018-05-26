d30ddd44f2a70f8eeee7e49c9cc21b126dae02f3
MasahiroAsaka <pedestrem.paradiso@gmail.com>
2018-05-26 18:41:02 +0900

    #2 Add Automatic Update
    MessagesControllerのindex action
    ・自動更新時のアクションを修正

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,23 +1,21 @@
 $(function(){
 
   function buildHTML(message){
-    var html_common =
-    `<div class="message" data-message-id="${message.id}">
-    <p class="upper-message__user-name">${message.name}</p>
-    <p class="upper-message__date">${message.created_at}</p>
-    <p class="lower-message__content">${message.text}</p>
-    </div>`
     var image = message.image
-    if (image == null) {
-      var html_without_image =
-      `${html_common}`
-    return html_without_image;
+    if (image) {
+      insertImage = `<img class="lower-message__image" src="${image}">`;
     }else{
-      var html_with_image =
-      `${html_common}
-      <img class="lower-message__imag" src=${message.image} alt="">`
-    return html_with_image;}
-  }
+      insertImage = '';
+    }
+    var html = `
+      <div class="message" data-message-id="${message.id}">
+        <p class="upper-message__user-name">${message.name}</p>
+        <p class="upper-message__date">${message.created_at}</p>
+        <p class="lower-message__content">${message.text}</p>
+        ${insertImage}
+      </div>`;
+    return html
+    }
 
   $('#the-form').on('submit', function(event){
     event.preventDefault();
@@ -47,19 +45,18 @@ $(function(){
     var interval = setInterval(function() {
     if (location.pathname.match(/\/groups\/\d+\/messages/)) {
       var messageId = ($('.message').data()) ? messageId = $('.message:last').data('message-id'): messageId = 0
-      console.log(messageId);
       $.ajax({
         url: location.href,
         type: 'GET',
-        data: { id: messageId },
+        data: {message: {id: messageId}},
         dataType: 'json',
       })
       .done(function(json){
         var insertHTML = '';
         json.messages.forEach(function(message){
-          if (message.id > messageId) {
+          if (message.id > messageId){
           insertHTML += buildHTML(message);
-          }
+        }
         });
         $('.group-message').append(insertHTML);
         $('.group-message').animate({ scrollTop: $('.group-message').get(0).scrollHeight },'fast');
